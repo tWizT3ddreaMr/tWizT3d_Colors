d3455acecb9f1d818661be93c9a1b4dae536ab85
tWizT3ddreaMr <tWizt3d.dreamr0729@gmail.com>
2022-08-21 11:11:23 -0500

    potential chat preview

--- src/main/java/me/tWizT3d_dreaMr/colors/Listeners/Chat.java
+++ src/main/java/me/tWizT3d_dreaMr/colors/Listeners/Chat.java
@@ -4,20 +4,30 @@
  import org.bukkit.event.EventPriority;
  import org.bukkit.event.Listener;
  import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatPreviewEvent;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.tWizT3d_dreaMr.colors.color;
  
- public class Chat implements Listener
+ @SuppressWarnings("deprecation")
+public class Chat implements Listener
  {
-   @EventHandler(ignoreCancelled=true, priority=EventPriority.LOWEST)
-   public void chat(AsyncPlayerChatEvent e) {
-	    if(color.isGrad(PlaceholderAPI.setPlaceholders(e.getPlayer(), "%vault_suffix%"))&&!e.getMessage().startsWith("#")&&!e.getMessage().startsWith("!")) {
-	    	e.setMessage(color.replaceAllGrad(e.getMessage()));
-	    	e.setMessage(PlaceholderAPI.setPlaceholders(e.getPlayer(), "%vault_suffix%").replace("!", "&")+e.getMessage());
-	    }
-		e.setMessage(color.ColorfyString(e.getMessage(), e.getPlayer(), "chat", "&"));
-	}
+	   @EventHandler(ignoreCancelled=true, priority=EventPriority.LOWEST)
+	   public void chatPreview( AsyncPlayerChatPreviewEvent e) {
+		    if(color.isGrad(PlaceholderAPI.setPlaceholders(e.getPlayer(), "%vault_suffix%"))&&!e.getMessage().startsWith("#")&&!e.getMessage().startsWith("!")) {
+		    	e.setMessage(color.replaceAllGrad(e.getMessage()));
+		    	e.setMessage(PlaceholderAPI.setPlaceholders(e.getPlayer(), "%vault_suffix%").replace("!", "&")+e.getMessage());
+		    }
+			e.setMessage(color.ColorfyString(e.getMessage(), e.getPlayer(), "chat", "&"));
+		}
+	   @EventHandler(ignoreCancelled=true, priority=EventPriority.LOWEST)
+	   public void chat(AsyncPlayerChatEvent e) {
+		    //if(color.isGrad(PlaceholderAPI.setPlaceholders(e.getPlayer(), "%vault_suffix%"))&&!e.getMessage().startsWith("#")&&!e.getMessage().startsWith("!")) {
+		    //	e.setMessage(color.replaceAllGrad(e.getMessage()));
+		    // 	e.setMessage(PlaceholderAPI.setPlaceholders(e.getPlayer(), "%vault_suffix%").replace("!", "&")+e.getMessage());
+		    //}
+			e.setMessage(color.ColorfyString(e.getMessage(), e.getPlayer(), "chat", "&"));
+		}
  }
 
 
