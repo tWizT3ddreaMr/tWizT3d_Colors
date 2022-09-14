96d07e1ac6dfc030cced56fec12645aeea4499af
tWizT3ddreaMr <tWizt3d.dreamr0729@gmail.com>
2022-09-14 10:56:39 -0500

    Update Chat.java

--- src/main/java/me/tWizT3d_dreaMr/colors/Listeners/Chat.java
+++ src/main/java/me/tWizT3d_dreaMr/colors/Listeners/Chat.java
@@ -1,15 +1,15 @@
- package me.tWizT3d_dreaMr.colors.Listeners;
+package me.tWizT3d_dreaMr.colors.Listeners;
  
- import org.bukkit.event.EventHandler;
- import org.bukkit.event.EventPriority;
- import org.bukkit.event.Listener;
- import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.tWizT3d_dreaMr.colors.color;
 
 public class Chat implements Listener
- {
+{
 	   @EventHandler(ignoreCancelled=true, priority=EventPriority.LOWEST)
 	   public void chat(AsyncPlayerChatEvent e) {
 		    if(color.isGrad(PlaceholderAPI.setPlaceholders(e.getPlayer(), "%vault_suffix%"))&&!e.getMessage().startsWith("#")&&!e.getMessage().startsWith("!")) {
@@ -18,6 +18,4 @@ public class Chat implements Listener
 		    }
 			e.setMessage(color.ColorfyString(e.getMessage(), e.getPlayer(), "chat", "&"));
 		}
- }
-
-
+}
